65c92449e7791151db85ada1fae1ff4618ca8e64
JavaScriptUser <3300739800@qq.com>
2020-12-15 16:32:43 +0800

    login

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import './plugins/element.js'
+import "./assets/css/base.css"
+import "./assets/js/flexible"
 
+axios.defaults.baseURL = 'http://localhost:3000/api'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <div class="login">欢迎登录</div>
+    <div class="demo-input-suffix">
+      <el-input
+        placeholder="请输入手机号"
+        prefix-icon="el-icon-phone"
+        v-model="userInfo.mobile"
+      >
+      </el-input>
+
+      <el-input
+        placeholder="请输入密码"
+        prefix-icon="el-icon-lock"
+        v-model="userInfo.password"
+        show-password
+      >
+      </el-input>
+      <div class="btn">
+        <el-button type="primary" @click="loginClick">登录</el-button>
+        <el-button type="primary">忘记密码</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      userInfo: {
+        mobile: 17637543547,
+        password: "1234567",
+      },
+    };
+  },
+  methods: {
+    async loginClick() {
+      let { res: data } = await this.$http.post("/login", this.userInfo);
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.login {
+  color: #409eff;
+  text-align: center;
+  margin-top: 2.666667rem;
+}
+.demo-input-suffix {
+  margin-top: 3rem;
+  padding: 0 16px;
+  .el-input {
+    margin: 0.533333rem 0;
+  }
+  .btn {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
